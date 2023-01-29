1c99127ae1dd15276a090ca57620cdee283c7669
M-Farag <mina.farag@icloud.com>
2023-01-29 20:17:07 +0200

    ADD Testing framework PHPUnit
    ADD PHPUnit config file

---
+++ tests/unit/SampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @covers Sample
+ */
+class SampleTest extends \PHPUnit\Framework\TestCase
+{
+    
+    public function test_this_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
