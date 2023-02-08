97dfddb3f409689776f39bb701cf9808507fe586
M-Farag <mina.farag@icloud.com>
2023-02-08 23:33:22 +0200

    ADD CURL_MULTI API Call setup

--- src/Transit.php
+++ src/Transit.php
@@ -15,13 +15,56 @@ class Transit {
      * Call
      * Perform parallel API calls using CURL Multi
      *
-     * @param array $request
+     * @param array $requests
      * @return array $results
      */
-    public function call(array $request) :array
+    public function call(array $requests) :array
     {
 
-        return [];
+        $channels = array();
+
+		$multi = curl_multi_init();
+		foreach ( $requests as $request ) {
+
+			$channel = curl_init();
+			curl_setopt( $channel, CURLOPT_URL, "uri" );
+			curl_setopt( $channel, CURLOPT_HTTPHEADER, array( "Authorization: Bearer token_if_needed" ) );
+			curl_setopt( $channel, CURLOPT_RETURNTRANSFER, true );
+
+			curl_multi_add_handle( $multi, $channel );
+
+			$channels[ "request_id" ] = $channel;
+		}
+
+			/**
+			 * Do Parallel API calls using CURL_MULTI
+			 */
+			$active = null;
+		do {
+			$mrc = curl_multi_exec( $multi, $active );
+		} while ( $mrc == CURLM_CALL_MULTI_PERFORM );
+
+		while ( $active && $mrc == CURLM_OK ) {
+			if ( curl_multi_select( $multi ) == -1 ) {
+				continue;
+			}
+
+			do {
+				$mrc = curl_multi_exec( $multi, $active );
+			} while ( $mrc == CURLM_CALL_MULTI_PERFORM );
+		}
+
+		foreach ( $channels as $channel ) {
+			// Getting the results of each request
+			$raw_message = curl_multi_getcontent( $channel );
+			// Processing the API call results and storing it to the $data list
+			$data[] =  json_decode( $raw_message, true );
+			curl_multi_remove_handle( $multi, $channel );
+		}
+
+		curl_multi_close( $multi );
+
+        return $data;
     }
 
 }
